873b92c8f2b9bca62cc95a383ed488491dc5a30a
Aishwarya <aishwaryaganga.s@gmail.com>
2025-06-24 14:23:57 -0400

    Headless

--- app/application.py
+++ app/application.py
@@ -11,7 +11,7 @@ class Application:
 
     def __init__(self):
         chrome_options = Options()
-        #chrome_options.add_argument("--headless=new")  # Required for GitHub Actions
+        chrome_options.add_argument("--headless=new")  # Required for GitHub Actions
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--disable-gpu")
