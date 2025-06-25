38c2d24f1338697b2f8e8e102dff5db447a7ea69
Aishwarya <aishwaryaganga.s@gmail.com>
2025-06-25 12:14:50 -0400

    Commit

--- Pages/search_page.py
+++ Pages/search_page.py
@@ -21,27 +21,27 @@ class SearchFor(Page):
         self.input_text("Toddler Toothbrush" , *self.SEARCH)
         self.click(*self.SEARCH_BUTTON)
         sleep(3)
-    #     #self.find_elements(*self.STAR).click()
-    #     checkbox = self.find_element(*self.CHECK_BOX)
-    #     actions = ActionChains(self.driver)
-    #     actions.click(checkbox).perform()
-    #     sleep(3)
-    #
-    #
-    # def hover_to_product(self):
-    #
-    #     #self.driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", self.PRODUCT)
-    #     element = self.wait.until(EC.presence_of_element_located(self.PRODUCT))
-    #     actions = ActionChains(self.driver)
-    #     actions.move_to_element(element).perform()
-    #     self.driver.execute_script("arguments[0].style.border='3px solid red'", element)
-    #     sleep(3)
-    #     element.click()
-    #
-    # def check_image(self):
-    #     images = self.find_elements(*self.IMAGES)
-    #     for image in images:
-    #         actions = ActionChains(self.driver)
-    #         actions.move_to_element(image).perform()
-    #         self.driver.execute_script("arguments[0].style.border='3px solid red';", image)
-    #         sleep(2)
+        #self.find_elements(*self.STAR).click()
+        checkbox = self.find_element(*self.CHECK_BOX)
+        actions = ActionChains(self.driver)
+        actions.click(checkbox).perform()
+        sleep(3)
+
+
+    def hover_to_product(self):
+
+        #self.driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", self.PRODUCT)
+        element = self.wait.until(EC.presence_of_element_located(self.PRODUCT))
+        actions = ActionChains(self.driver)
+        actions.move_to_element(element).perform()
+        self.driver.execute_script("arguments[0].style.border='3px solid red'", element)
+        sleep(3)
+        element.click()
+
+    def check_image(self):
+        images = self.find_elements(*self.IMAGES)
+        for image in images:
+            actions = ActionChains(self.driver)
+            actions.move_to_element(image).perform()
+            self.driver.execute_script("arguments[0].style.border='3px solid red';", image)
+            sleep(2)
