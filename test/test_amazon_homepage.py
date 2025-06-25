3c5f75b85e17475ae2362fa908cd052b97676b16
Aishwarya <aishwaryaganga.s@gmail.com>
2025-06-25 12:12:18 -0400

    Work1

--- test/test_amazon_homepage.py
+++ test/test_amazon_homepage.py
@@ -10,8 +10,8 @@ class TestOpenAmazonHomePage:
 
     def test_search_for(self,app):
         app.open_amazon.open_amazon_homepage()
-    #     app.open_amazon.continue_shopping()
-        app.search_product.search_for()
-        # app.search_product.hover_to_product()
-        # app.search_product.check_image()
+    # #     app.open_amazon.continue_shopping()
+    #     app.search_product.search_for()
+    #     app.search_product.hover_to_product()
+    #     app.search_product.check_image()
 
