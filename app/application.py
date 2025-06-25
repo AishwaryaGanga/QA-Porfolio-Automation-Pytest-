e236578dd83c316ef3eabf393b7976b272fa5d20
Aishwarya <aishwaryaganga.s@gmail.com>
2025-06-25 11:28:56 -0400

    First Test

--- app/application.py
+++ app/application.py
@@ -11,7 +11,7 @@ class Application:
 
     def __init__(self):
         chrome_options = Options()
-        #chrome_options.add_argument("--headless=new")
+        chrome_options.add_argument("--headless=new")
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--window-size=1920,1080")
         chrome_options.add_argument("--no-sandbox")
